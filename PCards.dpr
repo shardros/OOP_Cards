2f380fcea805e465cb85073eb7e54935c6b3e8de
shardros <shardro.s@gmail.com>
2018-02-28 20:29:16 +0000

    Added a hand unit

--- PCards.dpr
+++ PCards.dpr
@@ -7,18 +7,14 @@ program PCards;
 uses
   System.SysUtils,
   UCard in 'UCard.pas',
-  UDeck in 'UDeck.pas';
+  UPack in 'UPack.pas';
 
 var
   pack: tpack;
   card: tcard;
-  I : integer;
+
 begin
   pack := tpack.create;
   pack.shuffle;
-  for I := 0 to 51 do begin
-    card := pack.draw;
-    writeln(card.GetExplicitRank, ' of ', card.GetExplicitSuit);
-  end;
   readln;
 end.
---
+++ UHand.pas
@@ -0,0 +1,24 @@
+unit UHand;
+
+interface
+uses
+    UPack, UCard;
+
+type
+  Thand = array [0..51] of tcard;
+
+function getcontents: Thand;
+procedure drawfromdeck;
+
+implementation
+function getcontents: Thand;
+begin
+
+end;
+
+procedure drawfromdeck;
+begin
+
+end;
+
+end.
