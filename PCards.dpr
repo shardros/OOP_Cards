f47039972acb490e685af1b4fbae07805f7813bd
shardros <shardros@gmail.com>
2018-03-14 13:30:35 +0000

    Modified Pcards so that it didn't have all of the test code in it

--- PCards.dpr
+++ PCards.dpr
@@ -17,28 +17,7 @@ var
 begin
   randomize();
   init;
-  plar1 := 0;
-  plar2 := 1;
-  for i := 0 to 10000 do
-  begin
-    result := UGame.play;
-    if result = 1 then
-    begin
-      inc(plar1);
-    end
-    else
-    begin
-      if result = 2 then
-        inc(plar2);
-    end;
-    UGame.destroy;
-    UGame.init;
-  end;
 
-  writeln(plar1);
-  writeln(plar2);
-  readln;
-  init;
   test;
   destroy;
   readln;
