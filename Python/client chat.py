9af3a241fbbd69a0393887dbc0ed12e820eb3aba
Shepherd <ES128207@hillsroad.ac.uk>
2018-03-16 15:49:32 +0000

    Rewrote the python client program to be functional
    and added csv read and write functions, write will need to be adapted to
    be append.

--- Python/client chat.py
+++ Python/client chat.py
@@ -1,80 +1,40 @@
-#Import items, define the totalscore
-import random
-Totalscore = 0
-#Client setup
+import csv
+import time
 import os
 from socket import *
-#Get IP adress of target computer
+
 host = input("Enter sever computers IP addr:")
 port = 13000
-addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
+toDelphiFile = "ToDelphi.csv"
+fromDelphiFile = "ToPython.csv"
 
+packets = []
 
-#Ask for name, test that it contains at least one character.
-Name = input("What is your name? YOU MAY NOT HAVE SPACES IN YOUR NAME")
-while len(Name)<1:
-    print("Please enter a name.")
-    Name = input("What is your name? ")
-print("Hello, " + Name + ".")
-#Loop 15 times
-for x in range(0,15):
-    #Generate random numbers and the symbol
-    Symbol = random.randrange(1,3)
-    Number1 = random.randrange(1,99)
-    Number2 = random.randrange(1,99)
-    if Symbol == 1:
-        #If the symbol number is 1, use a +
-        Answer = Number1 + Number2
-        print(str(Number1) + " + " + str(Number2) + " = ")
-    elif Symbol == 2:
-        #If the symbol number is 2, use a -
-        Answer = Number1 - Number2
-        print(str(Number1) + " - " + str(Number2) + " = ")
-    elif Symbol == 3:
-        #If the symbol number is 3, use a *
-        Answer = Number1 * Number2
-        print(str(Number1) + " x " + str(Number2) + " = ")
-    #Ask for answers until an integer is given
-    while True:
-        try:
-            UserAnswer = int(input())
-            break
-        except ValueError:
-            print("That's not a valid answer. Answers should be integers and contain no text.")
-    if Answer == UserAnswer:
-        #If the user answer is the same as the actual answer, award a point and congratulate
-        Totalscore = Totalscore + 1
-        print("You got it right! You have got " + str(Totalscore) + " points so far!")
-    else:
-        #Otherwise, show the correct answer
-        print("You got it wrong, the answer was " + str(Answer) + ", you have " + str(Totalscore) + " points so far")
-#After the test, show their score
-print("The test is over, you got " + str(Totalscore) + " out of 15!")
-#Ask for valid class number and append data together
-while True:
-        try:
-            Class = int(input("What is your class number? "))
-            if Class>0 and Class<5:
-                break
-        except ValueError:
-            print("Please enter a valid number.")
-Data = Name + " " + str(Totalscore) + " " + str(Class)
-print(Data)
-print("Uploading score...")
+def Send(data, host, port):
+    addr = (host, port)
+    UDPSock = socket(AF_INET, SOCK_DGRAM)
+    UDPSock.sendto(Data.encode("utf8", 'ignore'), addr) 
 
+def Listen(host, port):
+     addr = (host, port)
+     UDPSock = socket(AF_INET, SOCK_DGRAM)
+     UDPSock.bind(addr)
+     rawData = UDPSock.recvfrom(buffer)
+     RawData = rawData.decode("utf-8")
+     UDPSock.close()
+     return rawData
 
-#--------------------------UPLOAD CODE-----------------------------
+def CSVRead(filename):
+    with open(filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            rows.append(row)
 
-try:
-    while True:
-        UDPSock.sendto(Data.encode("ASCII", 'ignore'), addr) #Encode and upload data
-        print("Data uploaded")
-        break
-except KeyboardInterrupt:
-    UDPSock.close()
-    os._exit(0)
+    return rows
 
+def CSVAppend(filename, data):
+    with open(filename, 'a') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(data)
 
-
-
+ 
--- Python/sever chat.py
+++ Python/sever chat.py
@@ -1,34 +1,25 @@
-import socket
-print("Students must enter the following as the IP: " +
-socket.gethostbyname(socket.gethostname()))
 import os
 from socket import *
+import csv
+import time
+
 host = ""
 port = 13000
-buf = 1024
-addr = (host, port)
-while True:
+buffer = 1024
+toDelphiFile = "ToDelphi.csv"
+fromDelphiFile = "ToPython.csv"
+
+def listen(host, port):
+     addr = (host, port)
      UDPSock = socket(AF_INET, SOCK_DGRAM)
      UDPSock.bind(addr)
-     print("Waiting to receive messages...")
-     (data, addr) = UDPSock.recvfrom(buf)
-     data = data.decode("ascii")
+     rawData = UDPSock.recvfrom(buffer)
+     RawData = rawData.decode("utf-8")
      UDPSock.close()
+     return rawData
 
-     x=0
-     while x != len(data):
-          if data[x] == " ":
-              Name = data[0:x]
-              print(Name)
-              break
-          x = x+1
-     data = data[x+1:len(data)]
-     x=0
-     while x != len(data):
-          if data[x] == " ":
-              Score = data[0:x]
-              print(Score)
-              break
-          x = x+1
-     Class = data[x+1:len(data)]
-     print(Class)
+
+print("Students must enter the following as the IP: " +
+socket.gethostbyname(socket.gethostname()))
+
+
