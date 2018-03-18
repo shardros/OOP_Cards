69ae820dda2c14875378c94e0ee41c13b4f21962
Edwin Shepherd <shardro.s@gmail.com>
2018-03-18 19:01:01 +0000

    Added limited csv support

--- python/main.py
+++ python/main.py
@@ -1,63 +1,12 @@
-from socket import *
-import time
+import CSVHandeling as Handeling
+import networking as net
 
 
-host = "192.168.1.6"
-port = 1234
-toDelphiFile = "ToDelphi.csv"
-fromDelphiFile = "ToPython.csv"
+ToPythonFileName = "ToPython.csv"
+ToDelphiFileName = "ToDelphi.csv"
 
-packets = []
+ToPython = Handeling.CSV(ToPythonFileName)
+ToDelphi = Handeling.CSV(ToDelphiFileName)
 
-
-class network:
-    host = ""
-    timeOut = 1
-    buffer = 1024
-
-    def __init__(self, host):
-        self.host = host
-
-    def Send(self, data):
-        addr = (host, port)
-        print("atempting conection to ", addr)
-        UDPSock = socket(AF_INET, SOCK_DGRAM)
-        UDPSock.sendto(data.encode("utf8", 'ignore'), addr)
-        print("sent")
-
-    def SendData(self,data):
-        self.Send(data)
-        response = self.Listen()
-        print("Got response ", response)
-        if (response == "ACK"):
-            return "RECIVED ACK"
-        else:
-            return "NET FAIL"
-
-    def Listen(self):
-        print("stated listen method")
-        addr = ("", port)
-        UDPSock = socket(AF_INET, SOCK_DGRAM)
-        UDPSock.bind(addr)
-        print("bound port; waiting....")
-        (data, hostAddr) = UDPSock.recvfrom(self.buffer)
-        data = data.decode("utf-8")
-        print("Got data: ", data)
-        return data
-
-    def ListenForData(self):
-        Data = self.Listen()
-        time.sleep(0.5)
-        self.Send("ACK")
-        return Data
-
-
-#from CSVHandeling import *
-
-
-n = network(host)
-print(n)
-print("sending test")
-print(n.SendData("I CAN USE NETWORKS!!!!!(*&F*()SA ¬¬!1`¬`"))
-
-
+print(ToDelphi.CSVRead())
+print(ToDelphi.MsgID)
--- python/networking.py
+++ python/networking.py
@@ -1,45 +1,49 @@
 from socket import *
-import threading
+import time
 
-host = "192.168.1.1"
-port = 13000
-toDelphiFile = "ToDelphi.csv"
-fromDelphiFile = "ToPython.csv"
-
-packets = []
-
+port = 1234
 
 class network:
     host = ""
     timeOut = 1
+    buffer = 1024
 
     def __init__(self, host):
         self.host = host
 
     def Send(self, data):
-        addr = (host, port)
-        UDPSock = socket(AF_INET, SOCK_DGRAM)
-        UDPSock.sendto(data.encode("utf8", 'ignore'), addr)
-        t = threading.Thread(target=self.Listen())
-        t.start(self)
-        t.join(self, timeout)
-        if ("ACK" == t):
-            return "Sent"
+        addr = (self.host, port)
+        Sock = socket(AF_INET, SOCK_DGRAM)
+        Sock.sendto(data.encode("utf8", 'ignore'), addr)
+        Sock.close()
+
+    def SendData(self, data):
+        self.Send(data)
+        response = self.Listen()
+        print("Got response ", response)
+        if (response == "ACK"):
+            return "RECIVED ACK"
         else:
-            return "Network failure"
+            return "NET FAIL"
 
     def Listen(self):
         addr = ("", port)
-        UDPSock = socket(AF_INET, SOCK_DGRAM)
-        UDPSock.bind(addr)
-        data = UDPSock.recvfrom(self.buffer)
+        Sock = socket(AF_INET, SOCK_DGRAM)
+        Sock.bind(addr)
+        (data, hostAddr) = Sock.recvfrom(self.buffer)
         data = data.decode("utf-8")
+        Sock.close()
         return data
 
     def ListenForData(self):
         Data = self.Listen()
+        time.sleep(0.5)
         self.Send("ACK")
         return Data
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """some clean up code probably should exist"""
+
 
 
 
