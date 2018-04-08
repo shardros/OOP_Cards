289ef6153f88a69988414d63fe3f84a4bdfc5f9d
shardros <shardro.s@gmail.com>
2018-04-08 18:58:23 +0100

    Added player object

--- Server side/index.js
+++ Server side/index.js
@@ -1,58 +1,60 @@
 var http = require('http');
 var url = require('url');
 
+function Player(num) {
+    var number = num;
+    var hand = [];
+    var ranksWon = [];
 
-function Game() {
-
-    this.deck =[]; //I know these should be private with get functions but this is easy
-    this.hands = [[]];
-    this.playerNum = 0;
-
-    var i;
-    for (i = 0; i < 52; i++) {
-        this.deck[i] = i;
+    this.addToHand = function(card) {
+        hand.push(card);
     }
 
-    this.createPlayer = function () {
-        if (playerNum < 7) {
-            this.hands.push([null]);
-            var number = this.dealToPlayer(playerNum);
-            this.playerNum++;
-            return number;
-        } else {
-            return false
-        }
+    this.removeFromHand = function (card) {
+        var pos = hand.indexOf(card);
+        hand.splice(pos, 1);
     }
+
+    this.getHand = function () {
+        result = hand;
+    }
+}
+
+function Game(numOfPlayers) {
+    var deck = [];
+    this.players = [];
+    var numberOfPlayers = numOfPlayers;
 
     this.dealToPlayer = function (player) {
-        var playerHand = [];
-
         for (x = 0; x < 8; x++) {
-            playerHand.push(this.deck.pop());
+            this.players[player].addToHand(deck.pop());
         }
-        this.hands[player] = playerHand;
-        return this.hands[player];
     }
-};
+
+    for (i = 0; i < 52; i++) {
+        deck[i] = i;
+    }
+
+    for (i = 0; i < numOfPlayers+1; i++) {
+        this.players[i] = new Player(i);
+        dealToPlayer(i);
+    }
+
+}
 
 var G = new Game();
-var i = 0
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var qdata = q.query;
+    var playerID = qdata.player;
     res.writeHead(200, {'Content-Type': 'text/html'});
-
-    if (qdata.player == null) {
-        var playerNo = G.createPlayer();
-        if (playerNo >= 0) {
-            res.write('Player:' + '<br/>' + playerNo + '<br/>');
-            res.write('Cards:' + '<br/>');
-            for (i = 0; i < 8; i++) {
-                res.write(G.hands[playerNo][i].toString() + '<br/>');
-            }
+    if (playerID >= 0) {
+        res.write('Player:' + '<br/>' + playerID + '<br/>');
+        res.write('Cards:' + '<br/>');
+        for (i = 0; i < 8; i++) {
+            res.write(G.players.getHand()[i].toString() + '<br/>');
         }
     }
-
     res.end();
 }).listen(8080);
